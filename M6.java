82ff1302e41dce5dee33a5e04ea6d9e14db50695
NikhitaYS <nikhita.23cs084@sode-edu.in>
2024-11-21 09:11:07 +0530

    Add files via upload

---
+++ M6.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oopj41;
+
+
+public class M6 {
+    public static void main(String[] args){
+        Tools t1=new Tools();
+        System.out.println(t1.reverse("Goutami"));
+    }
+    
+}
+    
+
