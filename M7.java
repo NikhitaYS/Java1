b6479b3165d9007ca40ce2f8a96ba9f75cb4f068
NikhitaYS <nikhita.23cs084@sode-edu.in>
2024-11-21 09:13:29 +0530

    Add files via upload

---
+++ M7.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oopj41;
+import pkg2.Tools3;
+
+public class M7 {
+    public static void main(String[] args){
+        Tools3 t1=new Tools3();
+        System.out.println(t1.reverse("Goutami"));
+    }
+    
+}
+    
+
