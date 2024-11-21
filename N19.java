6da0f7632aad35db096e6a88d668b8f2c53426ea
NikhitaYS <nikhita.23cs084@sode-edu.in>
2024-11-21 10:25:18 +0530

    Add files via upload

---
+++ N19.java
@@ -0,0 +1,23 @@
+
+package oopj41;
+public class N19 {
+    public static void main(String[] args){
+        //System.out.println("the even numbers");
+        for(int i=0;i<=5;i++){
+            System.out.println(i*2+" ");
+            System.out.println(" ");}
+         //System.out.println("Odd numbers");
+            for(int i=0;i<=5;i++){
+            System.out.println(i*2+1+" ");
+            System.out.println(" ");}
+            for(int j=3;j<101;j++){
+            for(int i=0;i<=10;i++){
+                System.out.println(i*j+" ");}
+            System.out.println();
+                
+                
+            
+            }
+        }
+    
+}
