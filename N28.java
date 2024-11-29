74dd20bf61028b5a6281ed16f08275ba19770f10
NikhitaYS <nikhita.23cs084@sode-edu.in>
2024-11-29 20:56:55 +0530

    Add files via upload

---
+++ N28.java
@@ -0,0 +1,27 @@
+package pkg1;
+
+public class N28 {
+    public static void main(String[] args){
+	byte b;
+	int i=257;
+	double d=323.142;
+
+	b=(byte)i;
+	System.out.println(i);
+	System.out.println(b);  
+	System.out.println();
+
+	b=(byte)d;
+	System.out.println(d);
+	System.out.println(b);  
+	System.out.println();
+
+	i=(int)d;
+	System.out.println(d);
+	System.out.println(i);  
+	System.out.println();
+
+
+
+    }
+}
