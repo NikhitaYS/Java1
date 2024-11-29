db5fe3553d01a5051947b2e041cb3cc77c21ff14
NikhitaYS <nikhita.23cs084@sode-edu.in>
2024-11-29 21:17:38 +0530

    Byte max value

---
+++ N32.java
@@ -0,0 +1,16 @@
+package pkg1;
+
+public class N32 {
+    public static void main(String[] args) {
+        System.out.println("Default values of Primitives");
+        byte b1=10;
+        byte b2=100;
+        byte b3=120;
+        byte b4=127;
+        
+        byte b6 = -10;
+        byte b7 = -100;
+        byte b8 = -127;
+        byte b9 = -128;     
+    }
+}
