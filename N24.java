fac625b3145b76ddbe0a7acfdaab78928d5ac995
NikhitaYS <nikhita.23cs084@sode-edu.in>
2024-11-29 20:51:28 +0530

    Add files via upload

---
+++ N24.java
@@ -0,0 +1,16 @@
+package pkg1;
+
+public class N24 {
+	public static void main(String[] args){
+	int lightspeed;
+	long days;
+	long seconds;
+	long distance;
+
+	lightspeed=186000;
+	days=1000;
+	seconds=days*24*60*60;
+	distance=lightspeed*seconds;
+	System.out.println(distance);
+	}
+}
